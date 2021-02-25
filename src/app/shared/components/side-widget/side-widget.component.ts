9c872a6868b66c82cc0c2dc5b190d4e02d122fa7
Anastasios Theodosiou <anastasios.theodosiou@gmail.com>
2021-02-25 23:03:10 +0200

    More adds

--- src/app/shared/components/side-widget/side-widget.component.ts
+++ src/app/shared/components/side-widget/side-widget.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class SideWidgetComponent implements OnInit {
 
   constructor() { }
-
+  description: string = 'Hey, I’m Anastasios. I’m a web developer living in Thessaloniki, Greece. I am a fan of web development and I am specialized in Frontend using technologies like Angular 2+, HTML5, CSS3, JavaScript, jQuery, Bootstrap, PrimeNG, Material etc. I am also dealing with some Backend stuff such as Node.js, Express, Mongoose, MongoDB.';
   ngOnInit() {
   }
 
