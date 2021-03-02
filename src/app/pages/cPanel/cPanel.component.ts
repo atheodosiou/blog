1b01d409b485c14f5c57eb15a64bda55bbbb0f15
Anastasios Theodosiou <anastasios.theodosiou@gmail.com>
2021-03-03 00:04:26 +0200

    Display articles on cpanel

--- src/app/pages/cPanel/cPanel.component.ts
+++ src/app/pages/cPanel/cPanel.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BlogPost } from 'src/app/shared/models/blog-post.model';
+import { Post } from 'src/app/shared/models/post.model';
+import { SdkService } from 'src/app/shared/services/sdk.service';
 
 @Component({
   selector: 'app-cPanel',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CPanelComponent implements OnInit {
 
-  constructor() { }
+  constructor(private sdk: SdkService) { }
   htmlContent: string;
+  result: Post;
+
   ngOnInit() {
+    this.sdk.getArticles({ limit: 10, offset: 0 }).subscribe(res => {
+      this.result = res;
+    }, error => { console.log(error) });
   }
 
 }
