3130fca8564aaa3fee2bfbc4c5dbcc27c9cf9e6b
atheodosiou <anastasios.theodosiou@gmial.com>
2021-03-07 18:52:16 +0200

    Remove unused code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,8 +11,6 @@ const routes: Routes = [
       post: PostResolver
     }
   },
-  { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
-  { path: 'cpanel', loadChildren: () => import('./pages/cPanel/cPanel.module').then(m => m.CPanelModule) },
   { path: '**', redirectTo: '/home' }
 ];
 
